11c872366c76b066e4847129454da7f8721967c8
Chris <zurad92@gmail.com>
2020-01-15 22:01:05 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var budgetController = (function(){
+    
+}());
+
+
+var appController  = (function(){
+    return {
+        test: function(){
+            console.log("TEST!!");
+        }
+    }
+}());
+
+
+var UIController = (function(){
+    
+}());
+
+
+
+appController.test();
