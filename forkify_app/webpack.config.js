090ebd0625dd74a855055fcfc700693c5749ce01
Chris <zurad92@gmail.com>
2020-01-29 21:34:37 +0100

    Update bundle.js dist directory

--- forkify_app/webpack.config.js
+++ forkify_app/webpack.config.js
@@ -3,7 +3,8 @@ const path = require('path');
 module.exports = {
     entry: './src/js/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'dist/js'),
         filename: 'bundle.js'
-    }
+    },
+    mode: 'development'
 };
