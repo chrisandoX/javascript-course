16db4992a69585691651a17c372ddbcc54bdb19a
Chris <zurad92@gmail.com>
2020-01-30 09:37:37 +0100

    Display search result to UI

--- forkify_app/src/js/index.js
+++ forkify_app/src/js/index.js
@@ -1,26 +1,29 @@
 import Search from "./models/Search";
-
+import { elements } from "./views/base";
+import * as searchView from "./views/SearchView";
 
 const state = {
 };
 
 const controlSearch = async  (e) => {
 
-    const query = "pizza" //TODO take it from view
+    const query = searchView.getInput();
     if(query){
         state.search = new Search(query);
     }
 
     // Prepare UI for results
+    searchView.clearResults();
 
     // Search for recepies
     await state.search.getResults();
 
     console.log(state.search.result);
+    searchView.renderResults(state.search.result);
 }
 
 
-document.querySelector(".search").addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch(e);
 })
